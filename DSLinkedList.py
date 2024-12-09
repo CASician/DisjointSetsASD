6bea747438b9b1f6ad2aea4a5cf698ff8c5c0665
CASician <cristian.sician@gmail.com>
2024-12-09 16:03:58 +0100

    fix: LinkedList ora ha senso

--- DSLinkedList.py
+++ DSLinkedList.py
@@ -11,39 +11,51 @@ class DSLinkedList:
 
     def find(self, data):
         node = self.sets[data]
-        return node.parent.data  # Restituisce il "capo" della lista
+        return node.root.data  # Restituisce il "capo" della lista
 
     def union(self, data1, data2):
         node1 = self.sets[data1]
         node2 = self.sets[data2]
 
         # Trova i rappresentanti di ciascun insieme
-        root1 = node1.parent
-        root2 = node2.parent
+        root1 = node1.root
+        root2 = node2.root
 
         # Se appartengono già allo stesso insieme, non fare nulla
         if root1 == root2:
             return
 
-        # Unisce l'insieme di root2 alla coda dell'insieme di root1
         root1.tail.next = root2  # Collegamento della lista
-        root1.tail = root2.tail  # Aggiorna la coda
 
-        # Imposta il parent di tutti gli elementi in root2 per puntare a root1
+        # Aggiustiamo seconda lista
         current = root2
         while current is not None:
-            current.parent = root1
+            current.root = root1
             current = current.next
+
+        # Aggiustiamo la prima lista
+        newTail = root2.tail
+        current1 = root1
+        while current1 is not root2:
+            current1.tail = newTail
+            current1 = current1.next
 
 # Esempio di utilizzo
 ds = DSLinkedList()
-ds.make_set(1)
-ds.make_set(2)
-ds.make_set(3)
-ds.make_set(4)
+for i in range(10):
+    ds.make_set(i)
 
-ds.union(1, 2)  # Unisce gli insiemi contenenti 1 e 2
-ds.union(3, 4)  # Unisce gli insiemi contenenti 3 e 4
-ds.union(2, 3)  # Unisce gli insiemi contenenti 2 (che include anche 1) e 3 (che include anche 4)
+ds.union(0, 1)  # Unisce gli insiemi contenenti 1 e 2
+# ds.union(2, 3)  # Unisce gli insiemi contenenti 3 e 4
+ds.union(1, 3)  # Unisce gli insiemi contenenti 2 (che include anche 1) e 3 (che include anche 4)
+ds.union(2, 5)
+ds.union(4, 6)
+ds.union(7, 8)
 
-print(ds.find(4))  # Output: il rappresentante dell'insieme (sarà 1 dopo le unioni)
+x = ds.sets
+
+for i in range(10):
+    if x[i].next is None:
+        print(x[i].data, x[i].root.data, "-", x[i].tail.data)
+    else:
+        print(x[i].data, x[i].root.data, x[i].next.data, x[i].tail.data)
--- Node.py
+++ Node.py
@@ -1,6 +1,6 @@
 class NodeLS:
-    def __init__(self, value):
-        self.value = value
-        self.parent = self  # Il capo iniziale è se stesso
+    def __init__(self, data):
+        self.data = data
+        self.root = self  # Il capo iniziale è se stesso
         self.next = None  # Puntatore al prossimo elemento nella lista concatenata
         self.tail = self  # Puntatore alla coda della lista
